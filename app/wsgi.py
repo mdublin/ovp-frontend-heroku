8e20cab32e8da34d8474d157c2bebda1058d1cca
mdublin <alegionofgenius@gmail.com>
2016-05-03 13:07:38 -0400

    edited wsgi.py to configure with production_heroku.py

--- app/wsgi.py
+++ app/wsgi.py
@@ -3,7 +3,7 @@ import os
 
 #config_name = os.environ.get('FLASK_CONFIG') or 'production_heroku'
 
-config_name = os.environ.get('FLASK_CONFIG') or 'production'
+config_name = os.environ.get('FLASK_CONFIG') or 'production_heroku'
 
 
 from app import create_app, db
