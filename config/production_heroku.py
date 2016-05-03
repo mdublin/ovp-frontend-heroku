d6cd359c7d41b85909429ac95aab92c2cdb98211
mdublin <alegionofgenius@gmail.com>
2016-05-03 19:01:27 -0400

    edit

--- config/production_heroku.py
+++ config/production_heroku.py
@@ -7,20 +7,20 @@ ADMINS = ['admin@test.com']
 DEBUG = True
 SECRET_KEY = 'top secret!'
 
-SQLALCHEMY_DATABASE_URI = "postgres://gkjrtbegisyndh:lF5JgQxWOvSMCBooTBUizlYFKK@ec2-107-20-174-127.compute-1.amazonaws.com:5432/dh9lfn7o0f3hv"
+#SQLALCHEMY_DATABASE_URI = "postgres://gkjrtbegisyndh:lF5JgQxWOvSMCBooTBUizlYFKK@ec2-107-20-174-127.compute-1.amazonaws.com:5432/dh9lfn7o0f3hv"
 
 
 
 urlparse.uses_netloc.append("postgres")
 url = urlparse.urlparse(os.environ["DATABASE_URL"])
 
-conn = pyscopg2.connect(
+conn = psycopg2.connect(
         database=url.path[1:],
         user=url.username,
         password=url.password,
         port=url.port
 )
 
-#SQLALCHEMY_DATABASE_URI = url
+SQLALCHEMY_DATABASE_URI = url
 
 
