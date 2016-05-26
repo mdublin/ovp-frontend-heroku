8fc7e2d389ac1c49c30efc77902967d534e3676c
mdublin <alegionofgenius@gmail.com>
2016-05-26 17:51:53 -0400

    file path

--- app/ovpAPI/DI/BC.py
+++ app/ovpAPI/DI/BC.py
@@ -25,7 +25,8 @@ def loadSecret():
     print("called loadSecret")
     try:
         #credsFile = open('./app/ovpAPI/DI/brightcove_oauth.json')
-        credsFile = open('brightcove_oauth.json')
+        #credsFile = open('brightcove_oauth.json')
+        credsFile = open('/app/ovpAPI/DI/brightcove_oauth.json')
         creds = json.load(credsFile)
         return creds
     except IOError as e:
